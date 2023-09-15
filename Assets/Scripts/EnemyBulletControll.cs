8ae133fc236bb93e22d1f05403e17a8608af363c
Kawaban <143968486+Kawaban@users.noreply.github.com>
2023-09-15 22:40:36 +0200

    Shield Animation added

--- Assets/Scripts/EnemyBulletControll.cs
+++ Assets/Scripts/EnemyBulletControll.cs
@@ -4,9 +4,26 @@ using UnityEngine;
 
 public class EnemyBulletControll : MonoBehaviour
 {
+    private GameObject shield;
+    [SerializeField] private float time=1f;
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.name == "Hero")
-            Destroy(gameObject);
+        {
+            shield = collision.transform.GetChild(2).gameObject;
+            if (shield.name == "shield_Edit")           
+                StartCoroutine("ShiledEffectCooldown");
+   
+        }
     }
+
+    private IEnumerator ShiledEffectCooldown()
+    {
+        shield.transform.Translate(new Vector3(0f, 0f, -1000f));
+        gameObject.transform.Translate(new Vector3(0f, 0f, 1000f));
+        yield return new WaitForSeconds(time);
+        shield.transform.Translate(new Vector3(0f, 0f, 1000f));
+        Destroy(gameObject);
+    }
+
 }
